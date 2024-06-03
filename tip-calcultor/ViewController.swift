95058207ea930b7e3e49a6c3f584ba75414449eb
kabikira <kabikirar2@protonmail.com>
2024-06-04 01:12:57 +0900

    first commit

---
+++ tip-calcultor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calcultor
+//
+//  Created by koala panda on 2024/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
